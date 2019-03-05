99fbf8ffde2b6f54aa97ae96ee449d3a131b4e4c
Ronan <ronan_int@hotmail.com>
2019-03-04 19:51:53 -0800

    Assignment3 - Finish

--- src/main/java/ca/ciccc/BankAccount.java
+++ src/main/java/ca/ciccc/BankAccount.java
@@ -11,7 +11,8 @@ public class BankAccount {
     private int transactionCount = 0;
     private String transactionText = "";
 
-    public BankAccount() { }
+    public BankAccount() {
+    }
 
     public BankAccount(double balance) {
         this.id = "test account";
@@ -75,22 +76,22 @@ public class BankAccount {
      * fees with greater than $0.00 remaining, the method will return {@code true}. If the balance cannot
      * afford all of the fees or has no money left, the balance is left as 0.0 and method returns
      * {@code false}.
-     *
+     * <p>
      * For example, given the following ca.ciccc.BankAccount object.
-     *
+     * <p>
      * ca.ciccc.BankAccount savings = new ca.ciccc.BankAccount("Jimmy");
      * savings.deposit(10.00);
      * savings.deposit(50.00);
      * savings.deposit(10.00);
      * savings.deposit(70.00);
-     *
+     * <p>
      * savings.transactionFee(5.00);
-     *
+     * <p>
      * The account would be deducted $5 + $10 + $15 + $20 for the four transactions, leaving a final
      * balance of $90.00. The method would return true.
-     *
+     * <p>
      * savings.transactionFee(10.00);
-     *
+     * <p>
      * Then the account would be deducted $10 + $20 + $30 + $40 for the four transactions, leaving
      * a final balance of $0.00. The method would return false.
      *
@@ -98,9 +99,18 @@ public class BankAccount {
      * @return true if there's enough balance, otherwise false
      */
     public boolean transactionFee(double fee) {
-        // TODO 2: Your code goes here.
-
-        return false;
+        // TODO 3: Your code goes here.
+        double transactionFee = 0;
+        int i = 1;
+        while (i <= transactionCount) {
+            transactionFee += fee * i;
+            i++;
+        }
+        if (balance > transactionFee){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     /**
@@ -109,11 +119,11 @@ public class BankAccount {
      * the money.
      * There is a $5.00 fee for transferring money, so this much must be deducted from the current
      * account's balance before any transfer.
-     *
+     * <p>
      * If `this` account object does not have enough money to make full transfer, then transfer
      * whatever money is left after the $5.00 fee is deducted. If this account has under $5.00 or amount is 0 or less,
      * no transfer should occur and neither account's state should be modified.
-     *
+     * <p>
      * If any amount of money is transferred, return {@code true}. Otherwise {@code false}.
      *
      * @param amount
@@ -121,9 +131,22 @@ public class BankAccount {
      * @return true if transferred any amount of money, otherwise false.
      */
     public boolean transfer(double amount, BankAccount other) {
-        // TODO 3: Your code goes here.
+        // TODO 2: Your code goes here.
+        if (balance <= 5){
+            return false;
+        }
+        if (amount <= 0){
+            return false;
+        }
+        this.balance = this.balance - 5;
+        double transfer = amount;
 
-        return false;
+        if (this.balance < amount) {
+            transfer = balance;
+        }
+        other.deposit(transfer);
+        balance -= transfer;
+        return true;
     }
 
     /**
@@ -140,7 +163,11 @@ public class BankAccount {
     @Override
     public String toString() {
         // TODO 1: Your code goes here.
-
-        return "";
+        String result = "";
+        result = String.format(name + ", $" + "%2.2f" , balance);
+        if (balance < 0) {
+            result=String.format(name + ", -$" + "%2.2f" , balance*-1);
+        }
+        return result;
     }
 }
